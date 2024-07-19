14c5dcb445be970c14041eeae9d2fa2c863b986e
GER2430 <gary.inter3.e01@gmail.com>
2024-07-18 19:44:22 -0600

    Reinicio del proyecto

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from routes.peliculas import peliculas
-from routes.peliculas import Usuario
 from flask_sqlalchemy import SQLAlchemy
 from utils.db import db
-from flask_login import LoginManager
 import os
 
-login_manager = LoginManager()
-app = Flask(__name__)
-login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Usuario.query.get(int(user_id))
+app = Flask(__name__)
+
+
+
 
 app.secret_key = "2430"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
--- routes/peliculas.py
+++ routes/peliculas.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required, current_user, login_user, logout_user
-from models.usuario import Usuario
 from models.pelicula import Pelicula
 from utils.db import db
 from werkzeug.utils import secure_filename
@@ -10,19 +8,14 @@ peliculas = Blueprint('peliculas', __name__)
 
 @peliculas.route("/")
 def index():
-    if not current_user.is_active:
-        return redirect(url_for ('peliculas.login'))
-    else:
-        peliculas = Pelicula.query.all()
-        return render_template('index.html', peliculas=peliculas)
+    peliculas = Pelicula.query.all()
+    return render_template('index.html', peliculas=peliculas)
 
 @peliculas.route("/new", methods=['GET'])
-@login_required
 def new_movie_page():
     return render_template('new.html')
 
 @peliculas.route("/add", methods=['post'])
-@login_required
 def add_movie():
      if request.method == 'POST':
         titulo = request.form['titulo']
@@ -33,9 +26,7 @@ def add_movie():
         sinopsis = request.form['sinopsis']
         imagen = request.files['imagen']
         if imagen:
-            # Asegurarse de que el nombre del archivo sea seguro
             filename = secure_filename(imagen.filename)
-            # Guardar la imagen en una ubicación específica (por ejemplo, carpeta "uploads" en el directorio estático)
             ruta_imagen = os.path.join('static/images', filename)
             imagen.save(ruta_imagen)
         
@@ -52,7 +43,6 @@ def add_movie():
         return "Invalid request method"
 
 @peliculas.route("/delete/<id>")
-@login_required
 def delete_movie(id):
     pelicula = Pelicula.query.get(id)
     db.session.delete(pelicula)
@@ -62,7 +52,6 @@ def delete_movie(id):
     return redirect(url_for('peliculas.index'))
 
 @peliculas.route("/update/<id>", methods = ['POST', 'GET'])
-@login_required
 def update_movie(id):
     pelicula = Pelicula.query.get(id)
 
@@ -80,41 +69,3 @@ def update_movie(id):
         return redirect(url_for("peliculas.index"))
     
     return render_template('update.html', pelicula=pelicula)
-
-@peliculas.route("/register", methods = ['GET', 'POST'])
-def registro():
-    if request.method == "GET":
-        return render_template("registro.html")
-    elif request.method == "POST":
-        nombre = request.form["nombre"]
-        username = request.form["username"]
-        password = request.form["password"]
-
-        usuario = Usuario(nombre, username, password)
-        db.session.add(usuario)
-        db.session.commit()
-
-        login_user(usuario)
-
-        return redirect(url_for('peliculas.index'))
-
-@peliculas.route("/login", methods = ['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        user = Usuario.query.filter_by(username=username).first()
-
-        if user and user.check_password(password):
-            login_user(user)
-            return redirect(url_for('peliculas.index'))
-        else:
-            return redirect(url_for('peliculas.login'))
-    return render_template('login.html')
-
-
-@peliculas.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('peliculas.index'))
